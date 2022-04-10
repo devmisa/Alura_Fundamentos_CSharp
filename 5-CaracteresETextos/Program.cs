ea83827ac25b68e5d505cfb1c3ab772b117e8583
Misael Lara Campos <misael-202@hotmail.com.br>
2022-04-10 10:25:20 -0300

    Adicionar arquivos de projeto.

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+
+            // character 
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+
+            string titulo = "Alura Cursos de tecnologia" + 2022;
+            string cursosProgramacao = @" 
+            - .NET 
+            - Javascript";
+                                
+               
+                        
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 12");   // Laços fortes encadeados
+
+
+            double valorInvestido = 1.000;
+            double fatorRendimento = 1.0036;
+
+            // FOR da contagem de anos
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                // FOR da contagem dos meses dentro do ano
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento; 
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            
+            Console.WriteLine("Ao término do investimento, você terá R$ " + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
